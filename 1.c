74eeb0d6ba751adc020403b7c006aa763cce810e
Shahriar Mahmud Tanim <157669438+tanimweb1@users.noreply.github.com>
2026-03-19 00:36:34 +0600

    Update 1.c

--- 1.c
+++ 1.c
@@ -11,5 +11,41 @@ for(int i = 0; i<=n;i++){
 }
 
 
+
+
+
+
+//prime or not 
+        int n, count = 0;
+        printf("Enter any positive number : ");
+        scanf("%d", &n);
+
+        if(n<=1){
+            count++;
+        }
+
+        for (int i = 2; i < n; i++)
+        {
+            if (n % i == 0)
+            {
+                count++;
+                break;
+            }
+        }
+
+        if (count == 0)
+        {
+            printf("%d is a prime\n",n);
+        }
+        else
+        {
+            printf("%d is not a prime\n",n);
+        }
+
+
+
+
+    
     return 0;
 }
+
