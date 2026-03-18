b2f1759ef203a5c681105d31cbd28d8c649f2de1
Shahriar Mahmud Tanim <157669438+tanimweb1@users.noreply.github.com>
2026-03-19 00:41:22 +0600

    Update 1.c

--- 1.c
+++ 1.c
@@ -45,6 +45,31 @@ for(int i = 0; i<=n;i++){
 
 
 
+
+
+
+
+// sum of digits 
+    int n, rem,sum=0;
+scanf("%d",&n);
+
+while(n!=0){
+
+    rem = n%10;
+    sum = sum + rem;
+    n = n/10;
+
+}
+
+printf("Sum of digits is %d",sum);
+
+
+
+
+
+
+
+    
     
     return 0;
 }
